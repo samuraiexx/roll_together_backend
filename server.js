289ba2eb2f93bf83ead18e419d1b90cc1592cb02
Mateus de Castro <samuraiexx@gmail.com>
2020-05-27 23:25:55 -0300

    Change to Socket.IO

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const { Server } = require('ws');
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -10,15 +10,11 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new Server({ server });
+const io = socketIO(server);
 
-wss.on('connection', (ws) => {
+io.on('connection', (socket) => {
   console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
